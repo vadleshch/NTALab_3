29ee35bcb8a1e9ba70081094deba6c6c66dcb2c3
vladle <165798388+vadleshch@users.noreply.github.com>
2026-05-06 00:47:44 +0300

    Add files via upload

---
+++ equation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NTALab_3
+{
+    internal class equation
+    {
+        public int[] C;
+        public int l;
+        public equation(int[] coefs, int lyshok)
+        {
+            C = coefs;
+            l = lyshok;
+        }
+    }
+}
